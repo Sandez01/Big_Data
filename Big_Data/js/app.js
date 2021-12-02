8b1f3f871e6acaa1814ae3b73b60bec6505069e9
Sandez01 <48538346+Sandez01@users.noreply.github.com>
2021-12-02 11:07:48 -0600

    Delete Big_Data directory

--- Big_Data/js/app.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator){
-    window.addEventListener('Load', () =>{
-        navigator.serviceWorker.register('/sw.js').then(() => {
-            console.log('Soy el service Worker registrado y funcionando.')
-        })
-    })
-}
